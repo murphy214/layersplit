1c17e4bf32eb38f99786dd64535c62e0fb0fc587
Bennett Murphy <murphy214@live.marshall.edu>
2017-06-25 14:59:07 -0400

    updates

--- layersplit.go
+++ layersplit.go
@@ -4,11 +4,8 @@ import (
 	"./polyclip"
 	"encoding/json"
 	"fmt"
-	//"math/rand"
-	"io/ioutil"
+	"math/rand"
 	m "mercantile"
-	p "newmod/poly_index"
-	"os/exec"
 	"strings"
 	"time"
 )
@@ -23,25 +20,14 @@ type Polygon struct {
 	Layers     map[string]string
 }
 
+// Point represents a point in space.
+type Output_Struct struct {
+	Polylist   []Polygon
+	Polystring string
+}
+
 type ResponseCoords2 struct {
 	Coords [][][]float64 `json:"coords"`
-}
-
-// gets the slope of two points along a line
-// if statement logic accounts for undefined corner case
-func get_slope(pt1 polyclip.Point, pt2 polyclip.Point) float64 {
-	if pt1.X == pt2.X {
-		return 1000000.0
-	}
-	return (pt2.Y - pt1.Y) / (pt2.X - pt1.X)
-}
-
-// iteroplates the position of y based on x of the location between two points
-// this function accepts m the slope to keep it from recalculating
-// what could be several hundred/thousand times between two points
-func interp(pt1 polyclip.Point, pt2 polyclip.Point, m float64, x float64) polyclip.Point {
-	y := (x-pt1.X)*m + pt1.Y
-	return polyclip.Point{x, y}
 }
 
 // gets the coordstring into a slice the easiest way I'm aware of
@@ -86,7 +72,35 @@ func get_extrema_coords(coords [][]float64) m.Extrema {
 	return m.Extrema{S: south, W: west, N: north, E: east}
 
 }
-func make_layer(polys [][]string, layername string) []Polygon {
+func make_polygon(coords [][][]float64) polyclip.Polygon {
+	thing2 := polyclip.Contour{}
+	things := polyclip.Polygon{}
+	for _, coord := range coords {
+		thing2 = polyclip.Contour{}
+
+		for _, i := range coord {
+			if len(i) == 2 {
+				// moving sign in 10 ** -7 pla
+				sign1 := rand.Intn(1)
+				factor1 := float64(rand.Intn(10)) * .00000001
+				if sign1 == 1 {
+					factor1 = factor1 * -1
+				}
+				sign2 := rand.Intn(1)
+				factor2 := float64(rand.Intn(10)) * .00000001
+				if sign2 == 1 {
+					factor2 = factor2 * -1
+				}
+				thing2.Add(polyclip.Point{X: i[0] + factor1, Y: i[1] + factor2})
+			}
+		}
+		things.Add(thing2)
+	}
+
+	return things
+}
+
+func Make_Layer(polys [][]string, layername string) []Polygon {
 	layer := []Polygon{}
 
 	c := make(chan []Polygon)
@@ -103,14 +117,6 @@ func make_layer(polys [][]string, layername string) []Polygon {
 			}
 			c <- templayer
 		}(polygonstrings, polyrow, c)
-		//for _, polygonstring := range polygonstrings {
-		//	go func(polygonstring string, polyrow []string, c chan<- Polygon) {
-		//		polygon := get_coords_json2(polygonstring)
-		//		polygonc := make_polygon(polygon)
-		//		extrema := get_extrema_coords(polygon[0])
-
-		//	}(polygonstring, polyrow, c)
-		//	layer = append(layer, Polygon{Area: polyrow[0], Layer: layername, Polygon: polygonc, Bounds: extrema, Polystring: polygonstring})
 
 	}
 	for ii := 0; ii < len(polys); ii++ {
@@ -122,116 +128,6 @@ func make_layer(polys [][]string, layername string) []Polygon {
 	}
 
 	return layer
-}
-
-func reverse(poly polyclip.Contour) polyclip.Contour {
-	if len(poly) <= 1 {
-		return poly
-	}
-	current := len(poly) - 1
-	newpoly := polyclip.Contour{}
-	for current != -1 {
-		newpoly.Add(poly[current])
-		current = current - 1
-	}
-	return newpoly
-}
-
-func Correct_coords(coords polyclip.Contour) polyclip.Contour {
-	needstobe := "positive"
-	count := 0
-	value := float64(0)
-	oldpt := polyclip.Point{}
-	var val string
-	var firstpt polyclip.Point
-	for _, pt := range coords {
-		if count == 0 {
-			count = 1
-			firstpt = pt
-		} else {
-			//value += (4096.0 - float64(oldpt[1]) + (4096.0 - float64(pt[1]))) * (float64(pt[0]) - float64(oldpt[0]))
-			value += (float64(oldpt.Y) + (float64(pt.Y))) * (float64(pt.X) - float64(oldpt.X))
-
-			count += 1
-		}
-		oldpt = pt
-	}
-	pt := firstpt
-
-	//value += (4096.0 - float64(oldpt[1]) + (4096.0 - float64(pt[1]))) * (float64(pt[0]) - float64(oldpt[0]))
-	value += (float64(oldpt.Y) + (float64(pt.Y))) * (float64(pt.X) - float64(oldpt.X))
-
-	fmt.Print(count, len(coords), "\n")
-	if value <= 0 {
-		val = "positive"
-	} else {
-		val = "negative"
-	}
-
-	if val == needstobe {
-		return coords
-	} else {
-		return reverse(coords)
-	}
-}
-
-func assert_winding_order(polygon polyclip.Polygon) polyclip.Polygon {
-	newpolygon := polyclip.Polygon{}
-	for _, cont := range polygon {
-		newpolygon.Add(Correct_coords(cont))
-	}
-	return newpolygon
-}
-
-func Within(poly1 polyclip.Contour, poly2 polyclip.Contour) bool {
-
-	boolval := true
-	for _, pt := range poly1 {
-		if poly2.Contains(pt) == false {
-			boolval = false
-			return boolval
-		}
-	}
-	return boolval
-}
-
-func make_polygons(polygon polyclip.Polygon) []polyclip.Polygon {
-
-	//e := polyclip.Polygon{}
-	mymap := map[int]polyclip.Polygon{}
-	for i, cont := range polygon {
-		mymap[i] = polyclip.Polygon{cont}
-
-	}
-
-	for oldi, oldcont := range polygon {
-		for i, cont := range polygon {
-			if (Within(oldcont, cont) == true) && (oldi != i) {
-				values := mymap[i]
-				values.Add(oldcont)
-				mymap[i] = values
-				delete(mymap, oldi)
-			}
-		}
-
-	}
-
-	//fmt.Print(len(mymap), len(polygon), "\n")
-	newlist := []polyclip.Polygon{}
-	for _, v := range mymap {
-		newlist = append(newlist, v)
-	}
-
-	//fmt.Print(len(e))
-	//fmt.Print(len(nonp), len(maxp), "\n")
-	return newlist
-}
-
-// finding the point that intersects with a given y
-func opp_interp(pt1 polyclip.Point, pt2 polyclip.Point, y float64) polyclip.Point {
-	m := get_slope(pt1, pt2)
-	x := ((y - pt1.Y) / m) + pt1.X
-	return polyclip.Point{x, y}
 }
 
 func check(poly1 Polygon, poly2 Polygon, typeval string, ch chan<- bool) {
@@ -257,7 +153,7 @@ func IsReachable(poly1 Polygon, poly2 Polygon, typeval string) bool {
 	return <-ch
 }
 
-func checkpt(bds m.Extrema, pt polyclip.Point, ok bool) bool {
+func Checkpt(bds m.Extrema, pt polyclip.Point, ok bool) bool {
 	if ok == true {
 		bds.N = bds.N + .00001
 		bds.E = bds.E + .00001
@@ -279,192 +175,60 @@ func checkpt(bds m.Extrema, pt polyclip.Point, ok bool) bool {
 	}
 }
 
-func raw_polygons(first Polygon, layer2 []Polygon) string {
-	bds := first.Bounds
-	finds := []Polygon{}
-	//fmt.Print("N:", bds.N, " S:", bds.S, "\n")
-	bds.N = bds.N + .0001
-	bds.E = bds.E + .0001
-	bds.S = bds.S - .0001
-	bds.W = bds.W - .0001
-	for _, polygon := range layer2 {
-		testbds := polygon.Bounds
-		//fmt.Print(bds.N, bds.S)
-
-		c1 := []float64{testbds.E, testbds.N}
-		c2 := []float64{testbds.W, testbds.N}
-		c3 := []float64{testbds.E, testbds.S}
-		c4 := []float64{testbds.W, testbds.S}
-		valbool := false
-
-		if (checkpt(bds, polyclip.Point{c1[0], c1[1]}, false) == false) && (checkpt(bds, polyclip.Point{c2[0], c2[1]}, false) == false) && (checkpt(bds, polyclip.Point{c3[0], c3[1]}, false) == false) && (checkpt(bds, polyclip.Point{c4[0], c4[1]}, false) == false) {
-			valbool = true
-		}
-
-		if ((((bds.N > testbds.N) && (bds.S < testbds.N)) || ((bds.N > testbds.S) && (bds.S < testbds.S))) && (((bds.E > testbds.E) && (bds.W < testbds.E)) || ((bds.E > testbds.W) && (bds.W < testbds.W)))) || (valbool == true) {
-			// && (((bds.E > testbds.E) && (bds.W < testbds.E)) || ((bds.E > testbds.W) && (bds.W < testbds.W))) {
-			//fmt.Print("S:", bds.S, " N:", bds.N, "test gap", testbds.S, testbds.N, polygon.Polystring[:100], "\n")
-			finds = append(finds, polygon)
-		}
-		//if ((bds.N < testbds.N) && (bds.S > testbds.N)) || ((bds.N < testbds.S) && (bds.S > testbds.S)) {
-		// && (((bds.E > testbds.E) && (bds.W < testbds.E)) || ((bds.E > testbds.W) && (bds.W < testbds.W))) {
-
-	}
-	newlist := []string{fmt.Sprintf(`%s,"%s"`, first.Area, first.Polystring)}
-	for _, newpoly := range finds {
-		newlist = append(newlist, fmt.Sprintf(`%s,"%s"`, newpoly.Area, newpoly.Polystring))
-	}
-
-	return strings.Join(newlist, "\n")
-}
-
-func raw_polygon(first polyclip.Polygon, area string) string {
-	if len(first) == 0 {
-		return ""
-	}
-	newlist := []string{}
-	for _, newpoly := range first {
-		newlist2 := []string{}
-		for _, row := range newpoly {
-			newlist2 = append(newlist2, fmt.Sprintf("[%f,%f]", row.X, row.Y))
-		}
-		newlist = append(newlist, fmt.Sprintf("[%s]", strings.Join(newlist2, ",")))
-	}
-	return fmt.Sprintf(`%s,"[%s]"`, area, strings.Join(newlist, ","))
-}
-
-func write(strval string) {
-	_ = ioutil.WriteFile("d.csv", []byte(strval), 0644)
-}
-
-func wepython(code []string) {
-	start := "import pandas as pd\nimport numpy as np\nimport mapkit as mk\n"
-
-	total := start + strings.Join(code, "\n")
-
-	_ = ioutil.WriteFile("a.py", []byte(total), 0644)
-	cmd := exec.Command("killall", "Safari")
-
-	cmd = exec.Command("freeport", "8000")
-
-	cmd = exec.Command("python", "a.py")
-	stdoutStderr, _ := cmd.CombinedOutput()
-
-	fmt.Printf("%s\n", stdoutStderr)
-
-}
-
-func make_const_polygons(finds []Polygon, first Polygon) []Polygon {
+// taking the found polygons and returning a list of each intersected polygon
+func Make_const_polygons(finds []Polygon, first Polygon, boolval bool) Output_Struct {
 	newlist := []Polygon{}
-	result := polyclip.Polygon{}
+	//result := polyclip.Polygon{}
 	total := polyclip.Polygon{}
 
-	newmap := map[string]string{}
-	newmap[first.Layer] = first.Area
-	if len(finds) > 0 {
-		newmap[finds[0].Layer] = "NONE"
-	}
+	// iterating through each found area
+	// iterating through each found area
 	for _, i := range finds {
-		amap := map[string]string{}
-		amap[first.Layer] = first.Area
 
-		amap[i.Layer] = i.Area
-
-		if IsReachable(first, i, "DIFFERENCE") == true {
-			result = first.Polygon.Construct(polyclip.INTERSECTION, i.Polygon)
-			for _, val := range result {
-				total.Add(val)
-			}
-
-			if len(result) == 0 {
-				if IsReachable(first, i, "DIFFERENCE") == true {
-					result = i.Polygon.Construct(polyclip.INTERSECTION, first.Polygon)
-					for _, val := range result {
-						total.Add(val)
-					}
-				}
-			}
-
+		//if IsReachable(first, i, "INTERSECTION") == true {
+		result := first.Polygon.Construct(polyclip.INTERSECTION, i.Polygon)
+		//}
+		for _, val := range result {
+			total.Add(val)
 		}
+
+		// adding the the result to newlist if possible
 		if len(result) != 0 {
-			bb := result.BoundingBox()
-			bds := m.Extrema{E: bb.Max.X, W: bb.Min.X, N: bb.Max.Y, S: bb.Min.Y}
-
-			newlist = append(newlist, Polygon{Polygon: result, Layers: amap, Bounds: bds})
+			amap := map[string]string{}
+			amap[first.Layer] = first.Area
+			amap[i.Layer] = i.Area
+			//fmt.Print(amap, "\n")
+			i.Polygon = result
+			i.Layers = amap
+			newlist = append(newlist, i)
+		} else {
+			//	fmt.Print("here\n", first.Polystring, "\n", i.Polystring, "\n")
+			//fmt.Print("here\n")
 		}
-	}
-	addtotal := true
-	if addtotal == true { //&& (len(total) != 0) {
-		newtotalpolygon := polyclip.Polygon{first.Polygon[0]}
-		for _, cc := range total {
-			newtotalpolygon.Add(cc)
-		}
-
-		bb := newtotalpolygon.BoundingBox()
-		bds := m.Extrema{E: bb.Max.X, W: bb.Min.X, N: bb.Max.Y, S: bb.Min.Y}
-
-		newlist = append(newlist, Polygon{Polygon: newtotalpolygon, Layers: newmap, Bounds: bds})
-
-	} else {
-		newtotalpolygon := polyclip.Polygon{}
-		newlist = append(newlist, Polygon{Polygon: newtotalpolygon, Layers: newmap})
 
 	}
 
-	return newlist
+	//fmt.Print(newlist, "\n")
+	if boolval == true {
+		return Output_Struct{Polylist: newlist}
+	} else if boolval == false {
+		return Output_Struct{Polystring: Make_Polygon_String(newlist)}
+	}
+	return Output_Struct{Polystring: Make_Polygon_String(newlist)}
 
 }
 
-func polystring(polys []Polygon, layername string) string {
-	newlist := []string{}
-	c := make(chan string)
-
-	keys := []string{}
-	for k := range polys[0].Layers {
-		keys = append(keys, k)
-	}
-	for _, row := range polys {
-		go func(row Polygon, layername string, c chan string) {
-			if layername == "Layers" {
-				val := row.Layers
-
-				newv := []string{}
-				for _, k := range keys {
-					newv = append(newv, val[k])
-				}
-				v := strings.Join(newv, ",")
-				c <- raw_polygon(row.Polygon, v)
-
-			} else {
-				c <- raw_polygon(row.Polygon, row.Layers[layername])
-			}
-		}(row, layername, c)
-	}
-	count := 0
-	total := 0
-	for range polys {
-		select {
-		case msg1 := <-c:
-			if count == 100 {
-				count = 0
-				total += 100
-				fmt.Print(total, "\n")
-			}
-			newlist = append(newlist, msg1)
-			count += 1
-		}
-	}
-	return strings.Join(newlist, "\n")
-}
-
-func make_layer_polygon(first Polygon, layer2 []Polygon) []Polygon {
+// makes a polygonial set in string in slice format
+func Make_layer_polygon(first Polygon, layer2 []Polygon, boolval bool) Output_Struct {
 	bds := first.Bounds
 	finds := []Polygon{}
 	//fmt.Print("N:", bds.N, " S:", bds.S, "\n")
-	bds.N = bds.N + .0001
-	bds.E = bds.E + .0001
-	bds.S = bds.S - .0001
-	bds.W = bds.W - .0001
+	//bds.N = bds.N + .0001
+	//bds.E = bds.E + .0001
+	//bds.S = bds.S - .0001
+	//bds.W = bds.W - .0001
+	bds = m.Extrema{W: bds.W - 0.000001, S: bds.S - 0.000001, E: bds.E + 0.000001, N: bds.N + 0.000001}
+
 	for _, polygon := range layer2 {
 		testbds := polygon.Bounds
 		//fmt.Print(bds.N, bds.S)
@@ -475,7 +239,7 @@ func make_layer_polygon(first Polygon, layer2 []Polygon) []Polygon {
 		c4 := []float64{testbds.W, testbds.S}
 		valbool := false
 
-		if (checkpt(bds, polyclip.Point{c1[0], c1[1]}, false) == false) && (checkpt(bds, polyclip.Point{c2[0], c2[1]}, false) == false) && (checkpt(bds, polyclip.Point{c3[0], c3[1]}, false) == false) && (checkpt(bds, polyclip.Point{c4[0], c4[1]}, false) == false) {
+		if (Checkpt(bds, polyclip.Point{c1[0], c1[1]}, false) == false) && (Checkpt(bds, polyclip.Point{c2[0], c2[1]}, false) == false) && (Checkpt(bds, polyclip.Point{c3[0], c3[1]}, false) == false) && (Checkpt(bds, polyclip.Point{c4[0], c4[1]}, false) == false) {
 			valbool = true
 		}
 
@@ -484,36 +248,21 @@ func make_layer_polygon(first Polygon, layer2 []Polygon) []Polygon {
 		}
 
 	}
-	return make_const_polygons(finds, first)
+
+	return Make_const_polygons(finds, first, boolval)
 }
 
-// Given two slices containg two different geographic layers
-// returns 1 slice representing the two layers in which there are split into
-// smaller polygons for creating hiearchies e.g. slicing zip codes about counties
-func Combine_Layers(layer1 []Polygon, layer2 []Polygon) []Polygon {
-	c := make(chan []Polygon)
-	newlist := []Polygon{}
-	for _, row := range layer1 {
-		go func(row Polygon, layer2 []Polygon, c chan<- []Polygon) {
-			a := make_layer_polygon(row, layer2)
-			//fmt.Print(a, "\n")
-			c <- a
-		}(row, layer2, c)
-	}
-	count := 0
-	total := 0
-	for range layer1 {
-		select {
-		case msg1 := <-c:
-			if count == 100 {
-				count = 0
-				total += 100
-				fmt.Print(total, "\n")
-			}
-			newlist = append(newlist, msg1...)
-			count += 1
-
+// this function will handle a polygon if it has
+// more than 5 contours
+// it will be used as the iteration value against the first polygon
+func Lint_Polygon(poly Polygon, first Polygon) Polygon {
+	bb := first.Polygon.BoundingBox()
+	newpoly := polyclip.Polygon{}
+	for _, cont := range poly.Polygon {
+		if bb.Overlaps(cont.BoundingBox()) == true {
+			newpoly.Add(cont)
 		}
 	}
-	return newlist
+	poly.Polygon = newpoly
+	return poly
 }
---
+++ wrapper.go
@@ -0,0 +1,215 @@
+package layersplit
+
+import (
+	pc "./polyclip"
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Point represents a point in space.
+type Config struct {
+	Output   string    // can be "csv" or "slice" currently
+	OutFile  string    // the output filename
+	Csv1     string    // csv file of layer to combine
+	Csv2     string    // csv file of layer to combine
+	Layer1   []Polygon // layer to combine if no csv given
+	Layer2   []Polygon // layer to combine if no csv given
+	Progress int       // steps to increment or show progress creating each new layer
+}
+
+// wrapper function for stock csv files to remove boiler plate bullshit
+// helper function for reading in ccsvs
+func Get_Csv(filename string) [][]string {
+	b, err := ioutil.ReadFile(filename) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(b)
+	r := csv.NewReader(strings.NewReader(str))
+
+	// Read all records.
+	data, _ := r.ReadAll()
+	return data[1:]
+
+}
+
+// making the string that corresponds to each polygon
+func Make_Each_Polygon(polygon pc.Polygon) string {
+	// making contour string
+	contourlist := []string{}
+	for _, cont := range polygon {
+		ptlist := []string{}
+		for _, pt := range cont {
+			ptlist = append(ptlist, fmt.Sprintf("[%f,%f]", pt.X, pt.Y))
+		}
+		contourlist = append(contourlist, fmt.Sprintf("[%s]", strings.Join(ptlist, ",")))
+	}
+	return fmt.Sprintf("[%s]", strings.Join(contourlist, ","))
+}
+
+// for a list of given polygons creates a polygon string
+func Make_Polygon_String(poly []Polygon) string {
+	polygonstringlist := []string{}
+	for _, pp := range poly {
+		// making json string from layermap
+		newlist := []string{}
+		mymap := pp.Layers
+		//fmt.Print(pp, "\n")
+
+		for k, v := range mymap {
+			if k == "Combined" {
+				newlist = append(newlist, v[1:len(v)-1])
+			} else {
+				newlist = append(newlist, fmt.Sprintf(`'%s':'%s'`, k, v))
+			}
+		}
+		jsonstring := fmt.Sprintf("{%s}", strings.Join(newlist, ","))
+		polygonstring := Make_Each_Polygon(pp.Polygon)
+		polystring := fmt.Sprintf(`"%s","%s"`, jsonstring, polygonstring)
+		polygonstringlist = append(polygonstringlist, polystring)
+	}
+	return strings.Join(polygonstringlist, "\n")
+}
+
+// Given two slices containg two different geographic layers
+// returns 1 slice representing the two layers in which there are split into
+// smaller polygons for creating hiearchies e.g. slicing zip codes about counties
+func Combine_Layers_Slice(layer1 []Polygon, layer2 []Polygon, progress int) []Polygon {
+	c := make(chan Output_Struct)
+	newlist := []Polygon{}
+	for _, row := range layer1 {
+		go func(row Polygon, layer2 []Polygon, c chan<- Output_Struct) {
+			a := Make_layer_polygon(row, layer2, true)
+			//fmt.Print(a, "\n")
+			c <- a
+		}(row, layer2, c)
+	}
+	count := 0
+	total := 0
+	for range layer1 {
+		select {
+		case msg1 := <-c:
+			if count == progress {
+				count = 0
+				total += progress
+				fmt.Printf("[%d/%d]\n", total, len(layer1))
+				//fmt.Print(total, "\n")
+			}
+			//for _, pol := range msg1 {
+			//	fmt.Print(pol.Layers, "\n")
+			//}
+			newlist = append(newlist, msg1.Polylist...)
+			count += 1
+
+		}
+	}
+	return newlist
+}
+
+// Given two slices containg two different geographic layers
+// returns 1 slice representing the two layers in which there are split into
+// smaller polygons for creating hiearchies e.g. slicing zip codes about counties
+func Combine_Layers_Csv(layer1 []Polygon, layer2 []Polygon, outfilename string, progress int) {
+	// creating channel
+	c := make(chan Output_Struct)
+
+	// creating file
+	ff, _ := os.Create(outfilename)
+	ff.WriteString("LAYERS,COORDS")
+	for _, row := range layer1 {
+		go func(row Polygon, layer2 []Polygon, c chan<- Output_Struct) {
+			a := Make_layer_polygon(row, layer2, false)
+			c <- a
+		}(row, layer2, c)
+	}
+
+	// iterating through each recieved channel output
+	count := 0
+	total := 0
+	for range layer1 {
+		select {
+		case msg1 := <-c:
+			if count == progress {
+				count = 0
+				total += progress
+				fmt.Printf("[%d/%d]\n", total, len(layer1))
+
+			}
+			ff.WriteString("\n" + msg1.Polystring)
+			count += 1
+
+		}
+	}
+
+}
+
+// given a configuration struct combines two layers
+// see configuration structure on line 9
+func Combine_Layers(configs Config) []Polygon {
+	// going through the configuration
+	layers := [][]Polygon{}
+	if len(configs.Csv1) != 0 {
+		layer := Make_Layer(Get_Csv(configs.Csv1), strings.Split(configs.Csv1, ".")[0])
+		layers = append(layers, layer)
+	}
+	if len(configs.Csv2) != 0 {
+		layer := Make_Layer(Get_Csv(configs.Csv2), strings.Split(configs.Csv2, ".")[0])
+		layers = append(layers, layer)
+	}
+	if len(configs.Layer1) != 0 {
+		layers = append(layers, configs.Layer1)
+	}
+	if len(configs.Layer2) != 0 {
+		layers = append(layers, configs.Layer2)
+	}
+
+	// evaluating layers
+	var layer1, layer2 []Polygon
+	if len(layers[0]) > len(layers[1]) {
+		layer1 = layers[1]
+		layer2 = layers[0]
+	} else {
+		layer1 = layers[0]
+		layer2 = layers[1]
+	}
+
+	// setting default progress increment if none given
+	if len(strconv.Itoa(configs.Progress)) == 1 {
+		configs.Progress = 100
+	}
+
+	// evaulating the two different types of output
+	outfilename := ""
+	if configs.Output == "csv" {
+		// evaluating outfilename
+		if (len(configs.OutFile) == 0) && (configs.Output == "csv") {
+			outfilename = "results.csv"
+		} else if configs.Output == "csv" {
+			outfilename = configs.OutFile
+		}
+		Combine_Layers_Csv(layer1, layer2, outfilename, configs.Progress)
+	} else if configs.Output == "layer" {
+		layer := Combine_Layers_Slice(layer1, layer2, configs.Progress)
+		newlist := []Polygon{}
+		for _, poly := range layer {
+			mymap := poly.Layers
+			newlist2 := []string{}
+			for k, v := range mymap {
+
+				newlist2 = append(newlist2, fmt.Sprintf(`'%s':'%s'`, k, v))
+			}
+			jsonstring := fmt.Sprintf("{%s}", strings.Join(newlist2, ","))
+			poly.Area = jsonstring
+			poly.Layer = "Combined"
+			newlist = append(newlist, poly)
+		}
+		return newlist
+	}
+	return []Polygon{}
+
+}
